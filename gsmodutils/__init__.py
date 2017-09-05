57ac825c21d1b409ce4a6a483443ac3b9986098c
James Gilbert <James.gilbert@nottingham.ac.uk>
2017-09-05 12:35:19 +0100

    Added our own model loader to move away from cameos which is mostly great but it makes testing more difficult.

--- gsmodutils/__init__.py
+++ gsmodutils/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, print_function
 from gsmodutils.project.interface import GSMProject
 from gsmodutils.project.project_config import ProjectConfig
+from gsmodutils.utils.io import load_model
 
 __version__ = '0.0.1'
---
+++ gsmodutils/utils/io.py
@@ -0,0 +1,31 @@
+import cobra
+import os
+
+
+def load_model(path, file_format=None):
+    """
+    Model loading that accepts multiple formats. Implemented to move away from dependency on cameo.load_model which,
+    while good for users, created problems in testing.
+    :str path: path to model
+    :str or None file_format: format model is imported in if None, attemps to guess
+    :return: cobra_model
+    """
+    if not os.path.exists(path):
+        raise IOError('File {} not found'.format(path))
+
+    if file_format is None:
+        # Guess the file_format from extension
+        file_format = os.path.splitext(path)[1][1:].strip().lower()
+
+    if file_format == "json":
+        cobra_model = cobra.io.load_json_model(path)
+    elif file_format == "yaml":
+        cobra_model = cobra.io.load_yaml_model(path)
+    elif file_format == "sbml":
+        cobra_model = cobra.io.read_sbml_model(path)
+    elif file_format in ["mat", "m", "matlab"]:
+        cobra_model = cobra.io.load_matlab_model(path)
+    else:
+        raise TypeError('Cannot load file format {}'.format(file_format))
+
+    return cobra_model
