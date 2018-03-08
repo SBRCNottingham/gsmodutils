b92d212b6c8cf369d6b3f8ee79d519aa2d352e4b
James Gilbert <James.gilbert@nottingham.ac.uk>
2018-03-08 15:29:18 +0000

    Model diff friendly HTML output now works/

--- gsmodutils/model_diff.py
+++ gsmodutils/model_diff.py
@@ -1,26 +1,40 @@
 from __future__ import print_function
-
 from cobra import Model
-
 from gsmodutils.utils import check_obj_sim, convert_stoich, equal_stoich
 
 
 class ModelDiff(dict):
-    """
-    A subclass of dict that has a nice, friendly html representation for use with jupyter
-    """
-    def __init__(self, *args, **kwargs):
+
+    def __init__(self, model_a=None, model_b=None, *args, **kwargs):
+        """
+        A subclass of dict that has a nice, friendly html representation for use with jupyter
+        :param cobra.Model model_a: left model (base model)
+        :param cobra.Model model_b: right model (new model)
+        """
+        self.model_a = model_a
+        self.model_b = model_b
+
         self.update(*args, **kwargs)
 
-        required_attr = ["metabolites", "reactions","genes",
+        required_attr = ["metabolites", "reactions", "genes",
                          "removed_reactions", "removed_metabolites", "removed_genes"]
 
         for k in required_attr:
             self.setdefault(k, [])
 
+        if self.model_b is not None and self.model_a is not None:
+            self._diff_models(self.model_a, self.model_b)
+
+        super(ModelDiff, self).__init__(*args, **kwargs)
+
     def _table_header(self):
+
+        title = ""
+        if self.model_b is not None and self.model_a is not None:
+            title = "Model diff between {} {}".format(self.model_a.id, self.model_b.id)
+
         info_str = """
-                Model diff with
+               {}
                 <table>
                     <tr>
                         <td> Removed reactions: </td>
@@ -48,6 +62,7 @@ class ModelDiff(dict):
                         <td> {} </td>
                     </tr>
                 """.format(
+            title,
             len(self["removed_reactions"]),
             len(self["removed_metabolites"]),
             len(self["removed_genes"]),
@@ -62,8 +77,16 @@ class ModelDiff(dict):
         if len(self["reactions"]):
             for reaction in self["reactions"]:
                 info_str += """
-                Added or changed reactions:
+                <strong> Added or changed reactions: </strong>
                 <table>
+                <th> Reaction id</th>
+                <th>name</th>
+                <th>gene_reaction_rule</th>
+                <th>subsystem</th>
+                <th>upper_bound</th>
+                <th>lower_bound</th>
+                <th>objective_coefficient</th>
+                <th>rstr</th>
                 <tr>
                     <td>{id}</td>
                     <td>{name}</td>
@@ -73,6 +96,8 @@ class ModelDiff(dict):
                     <td>{lower_bound}</td>
                     <td>{objective_coefficient}</td>
                     <td>{rstr}</td>
+                </tr>
+                <tr>
                     <td>
                         <table>
                             <th> Metabolite </th>
@@ -93,7 +118,28 @@ class ModelDiff(dict):
         info_str = ""
         if len(self["metabolites"]):
             info_str += """
+            <strong> Added/Changed Metabolites </strong>
+            <table>
+                <th> Metabolite id </th>
+                <th> name </th>
+                <th> compartment </th>
+                <th> charge </th>
+                <th> note </th>
+                <th> annotation </th>   
             """
+            for mb in self["metabolites"]:
+                info_str += """
+                    <tr>
+                        <td>{id}</td>
+                        <td>{name}</td>
+                        <td>{compartment}</td>
+                        <td>{charge}</td>
+                        <td>{notes}</td>
+                        <td>{annotation}</td>
+                    </tr>
+                """.format(**mb)
+
+            info_str += "</table>"
 
         return info_str
 
@@ -104,8 +150,25 @@ class ModelDiff(dict):
         info_str = ""
         if len(self["genes"]):
             info_str += """
+            <strong> Added/Changed Metabolites </strong>
+            <table>
+                <th> Gene id </th>
+                <th> Gene name </th>
+                <th> notes </th>
+                <th> functional </th>
+                <th> annotation </th>
             """
-
+            for gene in self["genes"]:
+                info_str += """
+                <tr>
+                    <td>{id}</td>
+                    <td>{name}</td>
+                    <td>{note}</td>
+                    <td>{functional}</td>
+                    <td>{annotation}</td>
+                </tr>
+                """.format(**gene)
+        info_str += "</table>"
         return info_str
 
     def _repr_html_(self):
@@ -119,64 +182,73 @@ class ModelDiff(dict):
                 <table>
                     <th> {0} </th>
                 """.format(title)
-                for id in self.get(attr):
+                for remid in self.get(attr):
                     istr += """
                     <tr> 
                         <td> {} </td>
                     </tr>
-                    """.format(id)
+                    """.format(remid)
 
                 istr += "</table>"
             return istr
 
         info_str += _rem_tpl("removed_metabolites", "Removed Metabolite")
+        info_str += "<br />"
         info_str += _rem_tpl("removed_reactions", "Removed Reactions")
+        info_str += "<br />"
         info_str += _rem_tpl("removed_genes", "Removed genes")
+        info_str += "<br />"
 
         info_str += self._reaction_table()
+        info_str += "<br />"
         info_str += self._metabolite_table()
+        info_str += "<br />"
         info_str += self._genes_table()
+        info_str += "<br />"
         return info_str
 
+    @staticmethod
+    def model_diff(model_a, model_b):
+        """
+        Returns a dictionary that contains all of the changed reactions between model a and model b
+        This includes any reactions or metabolites removed, or any reactions or metabolites added/changed
+        This does not say HOW a model has changed if reactions or metabolites are changed they are just included
+         with their new values
+        Diff assumes l -> r (i.e. model_a is the base model)
+        :param cobra.Model model_a:
+        :param cobra.Model model_b:
+        :return:
+        """
+        if not (isinstance(model_a, Model) and isinstance(model_b, Model)):
+            raise TypeError('Can only compare cobra models')
 
-def model_diff(model_a, model_b):
-    """
-    Returns a dictionary that contains all of the changed reactions between model a and model b
-    This includes any reactions or metabolites removed, or any reactions or metabolites added/changed
-    This does not say HOW a model has changed if reactions or metabolites are changed they are just included with their
-    new values
-    Diff assumes l -> r (i.e. model_a is the base model)
-    """
+        return ModelDiff(model_a, model_b)
 
-    if not (isinstance(model_a, Model) and isinstance(model_b, Model)):
-        raise TypeError('Can only compare cobra models')
+    def _diff_models(self, model_a, model_b):
+        """
 
-    metfields = ['formula', 'charge', 'compartment', 'name']
-    
-    diff = ModelDiff(
-        removed_metabolites=[],
-        removed_reactions=[],
-        reactions=[],
-        metabolites=[]
-    )
-    
-    for ma in model_a.metabolites:
-        # Find removed metabolites
-        try:
-            model_b.metabolites.get_by_id(ma.id)
-        except KeyError:
-            diff['removed_metabolites'].append(ma.id)
+        :param cobra.Model model_a:
+        :param cobra.Model model_b:
+        :return:
+        """
+        metfields = ['formula', 'charge', 'compartment', 'name']
+        for ma in model_a.metabolites:
+            # Find removed metabolites
+            try:
+                model_b.metabolites.get_by_id(ma.id)
+            except KeyError:
+                self['removed_metabolites'].append(ma.id)
 
-    for mb in model_b.metabolites:
-        # find added metabolites
-        # find if metabolite has changed at all
-        try:
-            ma = model_a.metabolites.get_by_id(mb.id)
-        except KeyError:
-            ma = None
-            
-        if ma is None or not check_obj_sim(ma, mb, metfields):
-                diff['metabolites'].append(
+        for mb in model_b.metabolites:
+            # find added metabolites
+            # find if metabolite has changed at all
+            try:
+                ma = model_a.metabolites.get_by_id(mb.id)
+            except KeyError:
+                ma = None
+
+            if ma is None or not check_obj_sim(ma, mb, metfields):
+                self['metabolites'].append(
                     dict(
                        id=mb.id,
                        notes=mb.notes,
@@ -187,65 +259,76 @@ def model_diff(model_a, model_b):
                        annotation=mb.annotation,
                     )
                 )
-    
-    reacfields = [
-        'lower_bound', 'upper_bound', 
-        'gene_reaction_rule', 'subsystem', 'name', 
-        'variable_kind' 
-    ]
-    for ra in model_a.reactions:
-        # reaction has been removed
-        try:
-            model_b.reactions.get_by_id(ra.id)
-        except KeyError:
-            diff['removed_reactions'].append(ra.id)
-    
-    for rb in model_b.reactions:
-        # reaction is new
-        try:
-            ra = model_a.reactions.get_by_id(rb.id)
-        except KeyError:
-            ra = None
-            
-        # reaction has changed or is new
-        if ra is None or not check_obj_sim(ra, rb, reacfields) or not equal_stoich(ra, rb):
-            diff['reactions'].append(
-                dict(
-                    id=rb.id,
-                    lower_bound=rb.lower_bound,
-                    upper_bound=rb.upper_bound,
-                    gene_reaction_rule=rb.gene_reaction_rule,
-                    subsystem=rb.subsystem,
-                    objective_coefficient=rb.objective_coefficient,
-                    name=rb.name,
-                    variable_kind=rb.variable_kind,
-                    metabolites=dict(convert_stoich(rb.metabolites)),
-                    rstr=repr(rb),
-                )
-            )
-    # Gene reaction rules are stored in reactions, however models also contains metadata for genes
-    genefields = ["name", "annotation", "notes"]
 
-    for ga in model_a.genes:
-        try:
-            model_b.genes.get_by_id(ga.id)
-        except KeyError:
-            diff['removed_genes'].append(ga.id)
+        reacfields = [
+            'lower_bound', 'upper_bound',
+            'gene_reaction_rule', 'subsystem', 'name',
+            'variable_kind'
+        ]
+        for ra in model_a.reactions:
+            # reaction has been removed
+            try:
+                model_b.reactions.get_by_id(ra.id)
+            except KeyError:
+                self['removed_reactions'].append(ra.id)
 
-    for gb in model_b.genes:
-        try:
-            ga = model_a.genes.get_by_id(gb.id)
-        except KeyError:
-            ga = None
+        for rb in model_b.reactions:
+            # reaction is new
+            try:
+                ra = model_a.reactions.get_by_id(rb.id)
+            except KeyError:
+                ra = None
+
             # reaction has changed or is new
-        if ga is None or not check_obj_sim(ga, gb, genefields):
-            diff['genes'].append(
-                dict(
-                    id=gb.id,
-                    name=gb.name,
-                    annotation=gb.annotation,
-                    notes=gb.notes,
-                    functional=gb.functional,
+            if ra is None or not check_obj_sim(ra, rb, reacfields) or not equal_stoich(ra, rb):
+                self['reactions'].append(
+                    dict(
+                        id=rb.id,
+                        lower_bound=rb.lower_bound,
+                        upper_bound=rb.upper_bound,
+                        gene_reaction_rule=rb.gene_reaction_rule,
+                        subsystem=rb.subsystem,
+                        objective_coefficient=rb.objective_coefficient,
+                        name=rb.name,
+                        variable_kind=rb.variable_kind,
+                        metabolites=dict(convert_stoich(rb.metabolites)),
+                        rstr=repr(rb),
+                    )
                 )
-            )
-    return diff
+        # Gene reaction rules are stored in reactions, however models also contains metadata for genes
+        genefields = ["name", "annotation", "notes"]
+
+        for ga in model_a.genes:
+            try:
+                model_b.genes.get_by_id(ga.id)
+            except KeyError:
+                self['removed_genes'].append(ga.id)
+
+        for gb in model_b.genes:
+            try:
+                ga = model_a.genes.get_by_id(gb.id)
+            except KeyError:
+                ga = None
+                # reaction has changed or is new
+            if ga is None or not check_obj_sim(ga, gb, genefields):
+                self['genes'].append(
+                    dict(
+                        id=gb.id,
+                        name=gb.name,
+                        annotation=gb.annotation,
+                        notes=gb.notes,
+                        functional=gb.functional,
+                    )
+                )
+
+
+def model_diff(model_a, model_b):
+    """
+    @depricated - use ModelDiff.model_diff(model_a, model_b)
+    Returns a ModelDiff dictionary that contains all of the changed reactions between model a and model b
+    This includes any reactions or metabolites removed, or any reactions or metabolites added/changed
+    This does not say HOW a model has changed if reactions or metabolites are changed they are just included with their
+    new values
+    Diff assumes l -> r (i.e. model_a is the base model)
+    """
+    return ModelDiff.model_diff(model_a, model_b)
